b39b36cd7bc070be6372f4d3747dc4255d1d3cf7
ColinOlivier <55018721+ColinOlivier@users.noreply.github.com>
2024-06-13 11:56:34 +0200

    fix file path

--- src/map/MapDrawer.hpp
+++ src/map/MapDrawer.hpp
@@ -11,22 +11,22 @@ struct MapDrawer
 {
 private:
     std::unordered_map<int, std::string> connectionIndexFilePathMap{
-        {0, "images/tilesCustom/path_00.png"},
-        {1, "images/tilesCustom/path_01.png"},
-        {2, "images/tilesCustom/path_02.png"},
-        {3, "images/tilesCustom/path_03.png"},
-        {4, "images/tilesCustom/path_04.png"},
-        {5, "images/tilesCustom/path_05.png"},
-        {6, "images/tilesCustom/path_06.png"},
-        {7, "images/tilesCustom/path_07.png"},
-        {8, "images/tilesCustom/path_08.png"},
-        {9, "images/tilesCustom/path_09.png"},
-        {10, "images/tilesCustom/path_10.png"},
-        {11, "images/tilesCustom/path_11.png"},
-        {12, "images/tilesCustom/path_12.png"},
-        {13, "images/tilesCustom/path_13.png"},
-        {14, "images/tilesCustom/path_14.png"},
-        {15, "images/tilesCustom/path_15.png"}
+        {0, "images/Map/tilesCustom/path_00.png"},
+        {1, "images/Map/tilesCustom/path_01.png"},
+        {2, "images/Map/tilesCustom/path_02.png"},
+        {3, "images/Map/tilesCustom/path_03.png"},
+        {4, "images/Map/tilesCustom/path_04.png"},
+        {5, "images/Map/tilesCustom/path_05.png"},
+        {6, "images/Map/tilesCustom/path_06.png"},
+        {7, "images/Map/tilesCustom/path_07.png"},
+        {8, "images/Map/tilesCustom/path_08.png"},
+        {9, "images/Map/tilesCustom/path_09.png"},
+        {10, "images/Map/tilesCustom/path_10.png"},
+        {11, "images/Map/tilesCustom/path_11.png"},
+        {12, "images/Map/tilesCustom/path_12.png"},
+        {13, "images/Map/tilesCustom/path_13.png"},
+        {14, "images/Map/tilesCustom/path_14.png"},
+        {15, "images/Map/tilesCustom/path_15.png"}
     };
     std::unordered_map<int, GLuint> _connectionIndexTextureIDMap;
 public:
