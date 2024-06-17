0f97fca0610505da62f935134cba7cd01ac8b9c9
Laurine-sl <laurine.suel@edu.esiee.fr>
2024-06-17 18:03:58 +0200

    tour ananas upload : on a deux tours youhou

--- src/tower/towerDrawer.cpp
+++ src/tower/towerDrawer.cpp
@@ -11,8 +11,11 @@
 
 void TowerDrawer::loadTowerTexture()
 {
-    img::Image tour{ img::load(make_absolute_path("images/Towers/Tour1.png", true), 4, true) };
-    _texture = loadTexture(tour);
+
+    img::Image tour{img::load(make_absolute_path("images/Towers/Tour1.png", true), 4, true)};
+    _texture1 = loadTexture(tour);
+    tour = {img::load(make_absolute_path("images/Towers/tour_ananas.png", true), 4, true)};
+    _texture2 = loadTexture(tour);
 }
 
 void TowerDrawer::setup()
@@ -20,7 +23,8 @@ void TowerDrawer::setup()
     loadTowerTexture();
 }
 
-void TowerDrawer::drawTowers(std::vector<Tower>& towersVector) {
+void TowerDrawer::drawTowers(std::vector<Tower> &towersVector)
+{
     for (size_t i = 0; i < towersVector.size(); i++)
     {
         glPushMatrix();
@@ -29,16 +33,11 @@ void TowerDrawer::drawTowers(std::vector<Tower>& towersVector) {
             towersVector[i].positionTower.y * (-2) + 1,
             0.0f);
         // glTranslatef(1.f, 1.f, 0.0f);
-        glScalef(0.4f, 0.47f, 0.4f);
-        draw_quad_with_texture(_texture);
-        // Tower tower{};
-        //  if(meter % (shootingRace*10)==0)
-        //  {
-
-        //  }
-        //  meter ++;
-
+        glScalef(0.3f, 0.35f, 0.3f);
+        if(towersVector[i].type == TowerType::basicTower)
+            draw_quad_with_texture(_texture1);
+        if(towersVector[i].type == TowerType::pineappleTower)
+            draw_quad_with_texture(_texture2);
         glPopMatrix();
     }
 }
-
